176592caad949ff902bf36f0ee2f509677639bc3
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-06-11 13:01:14 -0700

    Fix array updating for different default settings

--- src/frontend/components/DynamicForm.tsx
+++ src/frontend/components/DynamicForm.tsx
@@ -117,7 +117,8 @@ const DynamicForm = ({
     const { name, value, type, checked, files } = e.target;
 
     if (arrayKey !== null) {
-      const newArray = [...formState[arrayKey]];
+      const oldArray = formState[arrayKey]
+      const newArray = oldArray ? [...oldArray] : [];
       const resolvedValue = type === 'number' ? Number(value) : value;
       newArray[index] = resolvedValue;
       setFormState({ ...formState, [arrayKey]: newArray });
@@ -228,7 +229,7 @@ const DynamicForm = ({
 
       // Set to the minimum number of items if the length is locked
       const lockedLength = property?.minItems === property?.maxItems;
-      if (lockedLength) arrayValue = Array.from({ length: property.minItems }).map((_, index) => arrayValue[index] ?? fillerValue);
+      if (lockedLength) arrayValue = formState[key] = Array.from({ length: property.minItems }).map((_, index) => arrayValue[index] ?? fillerValue);
 
       // Check if the user can add or remove items
       const canAdd = !property?.maxItems || arrayValue.length < property.maxItems;
