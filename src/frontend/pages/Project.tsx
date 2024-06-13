433c6e59716ace3f509316d355e11b232ecce8fe
Garrett Michael Flynn <garrettmflynn@gmail.com>
2024-06-13 13:48:44 -0700

    Fix passing arguments to community function

--- src/frontend/pages/Project.tsx
+++ src/frontend/pages/Project.tsx
@@ -212,12 +212,10 @@ const Project: React.FC = () => {
       disabled: !segmented,
       content: <CommunityAnalysis 
         pipeline={loadedPipeline}
-        onFormSubmit={() => submitTab((props) => {
+        onFormSubmit={(props) => submitTab(() => {
             return showTerminalWhileRunning(async () => {
-
               await loadedPipeline.community(props) // Run community analysis
               await loadedPipeline.community_videos() // Creating community videos. NOTE: Will need additional consultation for how to proceed
-              
             }, 'Running community analysis + generating community videos')
           })}
       />
