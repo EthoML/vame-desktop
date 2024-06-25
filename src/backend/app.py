92cbe9dc8b7e69716cae1e6e9acca6db1f0cc9f8
Nathan Vieira Marcelino <nathan.v.marcelino@gmail.com>
2024-06-25 14:05:01 -0300

    fix missing save logs

--- src/backend/app.py
+++ src/backend/app.py
@@ -140,7 +140,7 @@ def get_project_path(
 def resolve_request_data(request):
     data = json.loads(request.data) if request.data else {}
     project_path = Path( data.pop("project") if "project" in data else Path(data["config"].parent) )
-    data["config"] = project_path / 'config.yaml' if "config" not in data else data["config"]
+    data["config"] = str(project_path / 'config.yaml') if "config" not in data else data["config"]
     return data, project_path
 
 
@@ -669,7 +669,7 @@ class Visualization(Resource):
             data, project_path = resolve_request_data(request)
             vame.visualization(
                 **data,
-                # save_logs=True
+                save_logs=True
             )
             return dict(result=get_visualization_images(project_path))
         except Exception as exception:
